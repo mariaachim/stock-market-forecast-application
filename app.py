53cca42cb6dccf82dfb7cb705c88a62fdbd51818
Maria <63523678+mariaachim@users.noreply.github.com>
2023-06-23 23:28:51 +0100

    added favourites table

--- app.py
+++ app.py
@@ -3,29 +3,32 @@
 # Started on 22nd June 2023
 
 from flask import Flask, render_template
-from models import db
+from models import db # local import
 
 from dotenv import load_dotenv
 
-import sys # standard library
+# standard libraries
 import os
 
+# loading environmental variables so i don't push credentials to github
 load_dotenv()
 username = os.getenv("DBUSER")
 password = os.getenv("DBPASSWORD")
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = f"mariadb+mariadbconnector://{username}:{password}@127.0.0.1/cs-nea"
+app.config["SQLALCHEMY_DATABASE_URI"] = f"mariadb+mariadbconnector://{username}:{password}@127.0.0.1/cs-nea" # database connection
 db.init_app(app)
 
 with app.app_context():
-    db.create_all()
+    db.create_all() # creates database if it doesn't exist already
     db.session.commit()
 
+# show login page automatically
 @app.route('/')
 @app.route('/login')
 def login():
     return render_template('login.html')
 
+# to run with python -m app
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=False)
--- models.py
+++ models.py
@@ -4,7 +4,7 @@ db = SQLAlchemy()
 
 class User(db.Model):
     __tablename__ = 'Credentials'
-    user_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, primary_key=True) # user_id is primary key
     username = db.Column(db.String(length=50), nullable=False, unique=True)
     password = db.Column(db.String(length=50), nullable=False)
 
@@ -13,7 +13,7 @@ class User(db.Model):
 
 class Company(db.Model):
     __tablename__ = 'Companies'
-    company_id = db.Column(db.Integer, primary_key=True)
+    company_id = db.Column(db.Integer, primary_key=True) # company_id is primary key
     name = db.Column(db.String(length=50), nullable=False)
     industry = db.Column(db.String(length=50), nullable=False)
     ceo = db.Column(db.String(length=50), nullable=False)
@@ -22,6 +22,14 @@ class Company(db.Model):
 
     def __repr__(self): # function for debugging purposes
         return f'<Company {self.name}>'
+    
+class Favourites(db.Model):
+    __tablename__ = 'Favourites'
+    user_id = db.Column(db.Integer, db.ForeignKey('Credentials.user_id'), primary_key=True, nullable=False)
+    company_id = db.Column(db.Integer, db.ForeignKey('Companies.company_id'), primary_key=True, nullable=False)
+
+    def __repr__(self): # function for debugging purposes
+        return f'<Favourites: User {self.user_id}, Company {self.company_id}>'
     
 def init_db():
     db.drop_all()
