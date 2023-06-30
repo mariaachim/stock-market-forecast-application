93102562daaf7ec37aaf81136e6f3ffc8bdd4405
Maria <63523678+mariaachim@users.noreply.github.com>
2023-07-01 00:42:53 +0100

    uhhhh

--- app.py
+++ app.py
@@ -2,7 +2,8 @@
 # Written by Maria Achim
 # Started on 22nd June 2023
 
-from flask import Flask, render_template, redirect, url_for, request, flash
+from flask import Flask, render_template, redirect, url_for, request, flash, session
+from flask_session import Session
 from models import db, Credentials # local import from models.py
 
 from dotenv import load_dotenv # to handle environment variables
@@ -18,7 +19,11 @@ key = os.getenv("KEY")
 
 app = Flask(__name__) # initialise flask application
 app.config["SQLALCHEMY_DATABASE_URI"] = f"mariadb+mariadbconnector://{dbusername}:{dbpassword}@127.0.0.1/cs-nea" # database connection
-app.config['SECRET_KEY'] = key
+app.config.update(
+    SESSION_TYPE='filesystem',
+    SECRET_KEY=key
+)
+Session(app)
 db.init_app(app) # initialise database
 
 with app.app_context():
@@ -28,7 +33,10 @@ with app.app_context():
 # show login page automatically
 @app.route('/')
 def index():
-    return redirect(url_for('login'))
+    if 'user' in session:
+        return render_template('index.html')
+    else:
+        return redirect(url_for('login'))
 
 def auth_index():
     return render_template('index.html')
@@ -37,15 +45,22 @@ def auth_index():
 def login():
     return render_template('login.html')
 
-@app.route('/login', methods=['POST']) # called when POST request is sent by interaction
+@app.route('/login', methods=['GET', 'POST']) # called when POST request is sent by interaction
 def login_post():
-    username = request.form.get('username')
-    password = request.form.get('psw')
-    is_user = Credentials.query.filter_by(username=username, password=password).first()
-    if not is_user:
-        flash('Please check login details') # reloads page and shows authentication failed
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('psw')
+        is_user = Credentials.query.filter_by(username=username, password=password).first()
+        if not is_user:
+            flash('Please check login details') # reloads page and shows authentication failed
+            return render_template('login.html')
+        else:
+            session['user'] = request.form.get('username')
+            session['passwd'] = request.form.get('psw')
+            return redirect(url_for('index')) # if user is authenticated, redirect to index page
+    else:
         return render_template('login.html')
-    return render_template('index.html') # if user is authenticated, redirect to index page
+# TODO redirects to /login when authenticated but with index.html
 
 @app.route('/register') # called when user navigates to register page
 def register():
